136e0907564e7cd994ea6ee2ae04162ebb00ba80
sushuai <516854715@qq.com>
2019-07-07 16:49:10 +0800

    增加数据建表

---
+++ database/test.sql
@@ -0,0 +1,24 @@
+create DATABASE videoright;
+use videoright;
+create table `user` (
+    `userid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `username` varchar (150) NOT NULL,
+    `userpassword` varchar (150) NOT NULL,
+    `email` varchar (150) NOT NULL,
+    `apitoken` varchar (150) NOT NULL,
+    `role` varchar (11) NOT NULL
+);
+create table `right` (
+    `rightid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `rightname` varchar (150) NOT NULL,
+    `publishers` varchar (150) NOT NULL,
+    `url` varchar (150) NOT NULL,
+    `email` varchar (150) NOT NULL,
+    `contentid` int (11) NOT NULL
+);
+create table `privacy` (
+    `privacyid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `url` int (11) NOT NULL,
+    `rightname` varchar (150) NOT NULL,
+    `contentid` int (11) NOT NULL
+);
