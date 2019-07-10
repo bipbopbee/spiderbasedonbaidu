9b8a8133f0b5f81978b49599d0bc46ec76cdd268
sushuai <516854715@qq.com>
2019-07-10 17:24:00 +0800

    修改SQL语句，增加UTF-8编码

--- database/test.sql
+++ database/test.sql
@@ -1,4 +1,4 @@
-create DATABASE videoright;
+create DATABASE videoright DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 use videoright;
 create table `user` (
     `userid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -7,25 +7,25 @@ create table `user` (
     `email` varchar (150) NOT NULL,
     `apitoken` varchar (150) NOT NULL,
     `role` varchar (11) NOT NULL
-);
-create table `right` (
+) DEFAULT CHARSET=utf8;
+create table `right`(
     `rightid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `rightname` varchar (150) NOT NULL,
     `url` varchar (150) NOT NULL,
     `email` varchar (150) NOT NULL,
     `contentid` int (11) NOT NULL
-);
-create table `right_tmp` (
+) DEFAULT CHARSET=utf8;
+create table `right_tmp`  (
     `rightid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `rightname` varchar (150) NOT NULL,
     `url` varchar (150) NOT NULL,
     `email` varchar (150) NOT NULL,
     `contentid` int (11) NOT NULL
-);
+) DEFAULT CHARSET=utf8;;
 create table `privacy` (
     `privacyid` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `url` varchar (150) NOT NULL,
     `rightname` varchar (150) NOT NULL,
     `contentid` int (11) NOT NULL
-);
+)DEFAULT CHARSET=utf8;
 insert into privacy (privacyid, url, rightname, contentid)  values (NULL, 'www.baidu.com','神奇', 22);
