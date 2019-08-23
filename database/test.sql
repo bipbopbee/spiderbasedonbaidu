115e63a57f8068b646e707c3fbaa90c7f73b4216
sushuai <516854715@qq.com>
2019-08-23 14:15:18 +0800

    增加searches表、增加searchengine表、修改privacy表

--- database/test.sql
+++ database/test.sql
@@ -45,14 +45,18 @@ create table `searches` (
     `searchnums` varchar (150) NOT NULL,
     `lastsearchtime` varchar (150) NOT NULL
 )DEFAULT CHARSET=utf8;
-<th>ID</th>
-        <th>影片名</th>
-        <th>类型</th>
-        <th>年代</th>
-        <th>关键词</th>
-        <th>搜索次数</th>
-        <th>最近一次搜索时间</th>
-insert into privacy (privacyid, url, rightname, contentid)  values (NULL, 'https://www.haokongbu.com/play/755951.html','急速追杀', 240303);
+create table `searchengine` (
+    `id` int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar (150) NOT NULL,
+    `keyword` varchar (150) NOT NULL,
+    `searchnums` varchar (150) NOT NULL
+)DEFAULT CHARSET=utf8;
+insert into privacy (privacyid, url, rightname, contentid)  values (NULL, 'http://ivi.bupt.edu.cn/hls/cctv3hd.m3u8','急速追杀', 240303);
 insert into user (userid, username, userpassword, email, apitoken, role)  values (NULL, 'abc','abc', '516854715@qq.com', 'LWtrKgMmLIeAWyyDUlLa', 'basic');
 insert into user (userid, username, userpassword, email, apitoken, role)  values (NULL, 'tom','abc', '123456@qq.com', 'LWtrKgMmLIeAWyyDUlLa', 'basic');
 insert into gongzhonghao (id, url, title, publishname, publishdate)  values (NULL, 'http://weixin.sogou.com/api/share?timestamp=1564649418&signature=qIbwY*nI6KU9tBso4VCd8lYSesxOYgLcHX5tlbqlMR8N6flDHs4LLcFgRw7FjTAOdoVu-Y9-TphrwUo0FGpF-l9TdjqBEcb81AuS67HX41kChMrMEa0swXwHStcyXbOylZKg3rvI-aQlxU12cfhFgoMcBPo*Y1qDo4PnwVgGP8e64LbM7d2tgZ50RhfBkgMijhSz*g*e3WNZKSCQlKOQJ5eW2hvVUQFW7MnCNSa6XKY=', '神奇', '知安视娱', '2019-08-01');
+insert into searches (id, name, type, year, keyword, searchnums, lastsearchtime)  values (NULL, '权力的游戏', 'film', '2017', '权力的游戏', '100', '2019-8-20 11:29:30');
+insert into searchengine (id, name, keyword, searchnums)  values (NULL, '百度', '急速追杀', '1000');
+update searchengine set searchnums= 1003 where keyword='急速追杀';
+alter table searchengine add column (searchtime char(150));
+alter table privacy add column (hosturl char(150));
