ee3298c356d87fb9238222398b7985c686ed7c81
sushuai <516854715@qq.com>
2019-09-18 13:39:50 +0800

    增加视频下载查询匹配模块

---
+++ videntify/mainloop.py
@@ -0,0 +1,41 @@
+#coding=utf-8
+import json
+import sys
+import datetime
+import time
+import threading
+from download import *
+sys.path.append("..")
+from database.opredis import *
+from database.dbHelper import *
+from database.searchenginedbHelper import *
+dbhelper = mySqlHelper()
+dbOperator = searchenginedbHelper()
+
+thread_num = 10
+
+#     table:
+#     keyword:
+#     title:
+#     detailurl:
+#     videourl:
+#     upname:
+
+def thread_function(keystr):
+    while True:
+        value = rpop(keystr)
+        if value == None:
+            time.sleep(5)
+            continue
+        data = json.loads(value)
+        print data
+        hosturl = data['detailurl']
+        videourl = data['videourl']
+        nowtime = str(datetime.datetime.now().microsecond)
+        thread_download(hosturl, videourl, nowtime)
+    pass
+
+if __name__ == '__main__':
+    for i in range(thread_num):
+        t = threading.Thread(target=thread_function, args=(key,))
+        t.start()
